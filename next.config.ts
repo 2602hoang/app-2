f6e468aec10cd23b7883c523b72010ba3b2d4009
2602hoang <vuhuyhoangboj@gmail.com>
2025-04-26 11:25:56 +0700

    fix2

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ module.exports = {
   basePath: "/app-2",
   // assetPrefix: "/app-2/",
   // trailingSlash: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/app-2/:path*",
-  //       destination: "/:path*",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/app-2/:path*",
+        destination: "/:path*",
+      },
+    ];
+  },
 };
