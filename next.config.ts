6148d08594e127fa7f51afae24172d0091f711ba
2602hoang <vuhuyhoangboj@gmail.com>
2025-05-01 15:47:58 +0700

    fix bo ne 1

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   basePath: "/app-2",
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/app-2/:path*",
-  //       destination: "/:path*",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/app-2/:path*",
+        destination: "/:path*",
+      },
+    ];
+  },
 };
